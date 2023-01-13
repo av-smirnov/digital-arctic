231091d4f8d1f42ff5607214327b94e118b298de
av-smirnov <121038267+av-smirnov@users.noreply.github.com>
2023-01-13 16:05:38 +0300

    2

--- app.py
+++ app.py
@@ -912,13 +912,13 @@ def settlement_map_plot(value, np, radius):
     radius_list = []
     for i, row in settl.iterrows():
         radius_list.append(round(GD((lat0, lon0), (row['lat'], row['lon'])).km, 2))
-    settl['Радиус'] = radius_list
+    settl['Расстояние от ' + np + ', км'] = radius_list
 
-    settl_table = settl[settl['Радиус'] <= float(radius)].iloc[:, [0, 6]].reset_index()
+    settl_table = settl[settl['Расстояние от ' + np + ', км'] <= float(radius)].iloc[:, [0, 6, 18]].reset_index()
     settl_table["№"] = [x for x in range(1, settl_table.shape[0]+1)]
 
     if len(settl_table.index) > 0:
-        table0 = dbc.Table.from_dataframe(settl_table.iloc[:, [3,1,2]], striped=True, bordered=True, hover=True)
+        table0 = dbc.Table.from_dataframe(settl_table.iloc[:, [4,1,2,3]], striped=True, bordered=True, hover=True)
     else:
         table0 = html.Div()
     summa = settl_table['Население, человек'].sum()
